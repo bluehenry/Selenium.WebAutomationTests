beea311a9dde0c017e07b9d3838a40a38c697941
Henry Zhang <henry.jijun@bhpbilliton.com>
2017-09-05 13:48:24 +0800

    Add Entity Framework demo

---
+++ WebApp.Test.Framework/Support/EntityFramework/DomainClasses/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Test.Framework.Support.EntityFramework.DomainClasses
+{
+    [Table("user")]
+    public class User
+    {
+        [Key]
+        public System.Guid user_id { get; set; }
+        public string user_name { get; set; }
+        public string pwd_hash { get; set; }
+        public Nullable<int> security_level { get; set; }
+        
+    }
+}
